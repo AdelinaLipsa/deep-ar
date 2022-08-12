69ab68f65d3db9bee9cb7bdec3b42adc54cddbdb
Adelina Lipsa <30749960+AdelinaLipsa@users.noreply.github.com>
2022-08-12 18:09:19 +0300

    wip

--- src/components/DeepPopUp.vue
+++ src/components/DeepPopUp.vue
@@ -62,7 +62,7 @@ export default {
       let product = this.productModel;
       // eslint-disable-next-line no-undef
       let deepAR = DeepAR({
-        licenseKey: '6b0c25cf43f87198a0066cdbcd7c7c6b8d2080dc2f88d29ca4ebf01fcd81b30e70ebb3abcfcad610',
+        licenseKey: 'f9bb1b4fecc41407fadb4c31bbfc0a237094ce8aaa2c618138ce31a51f70df7bb6f487c2743152e6',
         libPath: './lib',
         segmentationInfoZip: 'segmentation.zip',
         canvas: this.$refs.canvas,
